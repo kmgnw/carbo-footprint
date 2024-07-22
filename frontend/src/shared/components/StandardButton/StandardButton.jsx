69d7a02a0be0673714bf3e12cca5d75ed99bcd83
kmgnw <wathcpropropropro@gmail.com>
2024-07-22 12:14:35 +0900

    🚑️ Hovergrow 임포트 파일명 수정

--- frontend/src/shared/components/StandardButton/StandardButton.jsx
+++ frontend/src/shared/components/StandardButton/StandardButton.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { hoverGrow } from '../../animation/hovergrow'
+import { hoverGrow } from '../../animation/hoverGrow'
 
 function StandardButton({ width, height, title, onClick, backgroundColor="black", color='white'}) {
     return (

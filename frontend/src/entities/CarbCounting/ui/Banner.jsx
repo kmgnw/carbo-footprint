1e3289f3d2f28b25a09472658b43c01256207555
sengooooo <lesley023@naver.com>
2024-07-22 12:20:39 +0900

    🎨 STYLE. 아이콘 추가

--- frontend/src/entities/CarbCounting/ui/Banner.jsx
+++ frontend/src/entities/CarbCounting/ui/Banner.jsx
@@ -1,40 +1,46 @@
 import styled from "styled-components";
 import test from "../../../assets/testImg.svg";
-
-function Banner(){
-    return(
+import icon from "../../../assets/countingIcon.svg";
+function Banner() {
+  return (
     <Wrapper>
-        <Content>
-            당신의 식단 속<br/>탄수화물은<br/>얼마나 포함되어 있을까요?<br/>
-        </Content>
-        <Img src={test}/>
+      <Content>
+        당신의 식단 속<br />
+        <span style={{ color: "#E4CCFF" }}>탄수화물</span>은<br />
+        얼마나 포함되어 있을까요?
+        <br />
+      </Content>
+      <Img src={icon} />
     </Wrapper>
-    )
+  );
 }
 
 export default Banner;
 
 const Wrapper = styled.div`
-    width: 100%;
-    height: 12rem;
-    background: var(--Primary-color1, #EF6038);
-    padding : 0 2rem 0 2rem;
-    display: flex;
-`
+  position: relative;
+  width: 100%;
+  height: 12rem;
+  background: var(--Primary-color1, #ef6038);
+  padding: 0 2rem 0 2rem;
+  display: flex;
+`;
 
 const Content = styled.div`
-    width: 17.8rem;
-    color: var(--White, #FFF);
-font-family: "Noto Sans KR";
-font-size: 16px;
-font-style: normal;
-font-weight: 700;
-line-height: 150%; /* 24px */
-padding-top: 2.4rem;
-`
+  width: 17.8rem;
+  color: var(--White, #fff);
+  font-family: "Noto Sans KR";
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%; /* 24px */
+  padding-top: 2.4rem;
+`;
 
 const Img = styled.img`
-    width: 8rem;
-    height: 8rem;
-    margin: 2rem 0 0 13.2rem;
-`
+  position: absolute;
+  width: 14rem;
+  height: 11rem;
+  right: 0;
+  top: 1.2rem;
+`;
